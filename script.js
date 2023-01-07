11c9d91de1c07e407843e02ef4ac6b3e3c4f5e46
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-01-07 16:06:04 +0530

    chnges

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /** @format */
-
 let arr = [
   { id: 1, name: "john", age: "18", profession: "developer" },
   { id: 2, name: "jack", age: "20", profession: "developer" },
@@ -8,20 +7,48 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee.name);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee.name);
+    }
+  });
 }
 
 function addData() {
   //Write your code here
+  let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+
+  console.log(employees);
 }
 
 function removeAdmin() {
   //Write your code here
+  let updatedEmployees = arr.filter(
+    (employee) => employee.profession !== "admin"
+  );
+
+  console.log(updatedEmployees);
 }
 
 function concatenateArray() {
   //Write your code here
+  let newEmployees = [
+    { name: "vijay", profession: "developer", id: 1 },
+    { name: "anuska", profession: "designer", id: 2 },
+    { name: "rahul", profession: "manager", id: 3 },
+  ];
+
+  let allEmployees = arr.concat(newEmployees);
+
+  console.log(allEmployees);
 }
